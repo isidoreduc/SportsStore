4bdf6e3e042d37900a847318508121fcd45b7d91
isidoreduc <isidoreducasse79@gmail.com>
2020-06-25 17:14:44 +0200

    pagination in the controller and view model for pagination

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -10,11 +10,20 @@ namespace SportsStore.Controllers
     public class HomeController : Controller
     {
         private IStoreRepository _repo;
+        public int PageSize = 4; // 4 products/page
 
         public HomeController(IStoreRepository repo)
         {
             _repo = repo;
         }
-        public IActionResult Index() => View(_repo.Products);
+
+        //public IActionResult Index() => View(_repo.Products);
+
+        public ViewResult Index(int productPage = 1) => 
+            View(_repo.Products                                      // get products from repo
+                .OrderBy(p => p.ProductID)                           // order by id
+                .Skip((productPage - 1) * PageSize)                  // skip products occurring before the start of the current page
+                .Take(PageSize)                                      // take 4 products
+        );
     }
 }
