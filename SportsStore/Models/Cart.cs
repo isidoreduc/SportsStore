408d66c961b9c5facd439c8717e680d4892f10f8
isidoreduc <isidoreducasse79@gmail.com>
2020-06-27 17:23:05 +0200

    created a storage-aware Cart class

--- SportsStore/Models/Cart.cs
+++ SportsStore/Models/Cart.cs
@@ -9,7 +9,7 @@ namespace SportsStore.Models
     {
         public List<CartLine> Lines { get; set; } = new List<CartLine>();
         
-        public void AddItem(Product product, int quantity)
+        public virtual void AddItem(Product product, int quantity)
         {
             // identify the line (and its corresponding product)
             CartLine line = Lines
@@ -32,13 +32,13 @@ namespace SportsStore.Models
             }
         }
         
-        public void RemoveLine(Product product) =>
+        public virtual void RemoveLine(Product product) =>
             Lines.RemoveAll(l => l.Product.ProductID == product.ProductID);
         
         public decimal ComputeTotalValue() =>
             Lines.Sum(e => e.Product.Price * e.Quantity);
         
-        public void Clear() => Lines.Clear();
+        public virtual void Clear() => Lines.Clear();
     }
     public class CartLine
     {
