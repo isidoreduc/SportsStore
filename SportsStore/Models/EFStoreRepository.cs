25da73c1619510d1296b9992acbaee9e33689795
isidoreduc <isidoreducasse79@gmail.com>
2020-06-28 14:37:26 +0200

    crud for products - updating the repository code

--- SportsStore/Models/EFStoreRepository.cs
+++ SportsStore/Models/EFStoreRepository.cs
@@ -14,5 +14,21 @@ namespace SportsStore.Models
             _dbContext = dbContext;
         }
         public IQueryable<Product> Products => _dbContext.Products;
+
+        public void CreateProduct(Product p)
+        {
+            _dbContext.Add(p);
+            _dbContext.SaveChanges();
+        }
+        public void DeleteProduct(Product p)
+        {
+            _dbContext.Remove(p);
+            _dbContext.SaveChanges();
+        }
+        public void SaveProduct(Product p)
+        {
+            _dbContext.SaveChanges();
+        }
+
     }
 }
